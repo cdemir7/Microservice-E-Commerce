693eefa7eff87a44a4b8de33adb0e9a0f9b4c288
Cihan DEMİR <56312037+cdemir7@users.noreply.github.com>
2024-02-06 20:01:00 +0300

    Create | Create Model Mapper in common package

---
+++ common-package/src/main/java/com/example/commonpackage/utils/constants/Paths.java
@@ -0,0 +1,9 @@
+package com.example.commonpackage.utils.constants;
+
+public class Paths {
+    public static final String ConfigurationBasePackage = "com.example.commonpackage.configuration";
+
+    public static class Product{
+        public static final String ServiceBasePackage = "com.example.productservice";
+    }
+}
--- product-service/src/main/java/com/example/productservice/business/concretes/ProductManager.java
+++ product-service/src/main/java/com/example/productservice/business/concretes/ProductManager.java
@@ -1,5 +1,6 @@
 package com.example.productservice.business.concretes;
 
+import com.example.commonpackage.configuration.mapper.ModelMapperConfig;
 import com.example.productservice.business.abstracts.ProductService;
 import com.example.productservice.business.dto.requests.create.CreateProductRequest;
 import com.example.productservice.business.dto.requests.update.UpdateProductRequest;
@@ -9,6 +10,7 @@ import com.example.productservice.business.dto.responses.get.GetProductResponse;
 import com.example.productservice.business.dto.responses.update.UpdateProductResponse;
 import com.example.productservice.repository.ProductRepository;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,6 +20,7 @@ import java.util.UUID;
 @AllArgsConstructor
 public class ProductManager implements ProductService {
     private final ProductRepository repository;
+    private final ModelMapper mapper;
 
     @Override
     public List<GetAllProductsResponse> getAll() {
